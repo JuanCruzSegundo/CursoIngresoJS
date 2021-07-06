114c2c8d28b164a7851e29b51985516db6566937
JuanCruzSegundo <segu430@gmail.com>
2021-07-06 00:16:08 -0300

    ashe

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,16 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var nombre;
+	var mensaje;
+
+	nombre = prompt("Please enter your name", "Elvis Presley");
+	//nombre = "aaa";
+
+
+	mensaje="Su nombre es "+nombre;
+	//alert(mensaje);
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,27 @@
 /*
+Autor: Juan Cruz Segundo
+Ej: 05
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	var nombre;
+	var edad;
+	var mensaje;
+
+	//nombre="José";
+	nombre=document.getElementById("txtIdNombre").value;
+	//edad="66";
+	edad=document.getElementById("txtIdEdad").value;
+
+	mensaje="Usted se llama "+nombre+" y tiene "+edad +" años"
+
+	/*mensaje="Usted se llama ";
+	mensaje=mensaje+nombre;
+	mensaje=mensaje+ " y tiene ";
+	mensaje=mensaje+" edad";*/
+
+
+	alert(mensaje);
 }
 
